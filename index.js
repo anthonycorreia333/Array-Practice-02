4f77f9377eb52f07ca04ec409a5aa1abfc35ae2c
Anthony Correia <anthony.correia333@yahoo.com>
2017-03-08 16:57:45 -0800

    practice session complete

---
+++ index.js
@@ -0,0 +1,70 @@
+ const people = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 }
+    ];
+
+    const comments = [
+      { text: 'Love this!', id: 523423 },
+      { text: 'Super good', id: 823423 },
+      { text: 'You are the best', id: 2039842 },
+      { text: 'Ramen is my fav food ever', id: 123523 },
+      { text: 'Nice Nice Nice!', id: 542328 }
+    ];
+
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+    const isAdult = people.some(person => {
+      const currentYear = (new Date()).getFullYear();
+      if(currentYear - person.year >= 19){
+          return true;
+        }
+    });
+    console.log({isAdult});
+
+
+    // Array.prototype.every() // is everyone 19 or older?
+    // AC: used ternary conditional statement instead of if()
+    const allAdult = people.every(person => {
+      const currentYear = (new Date()).getFullYear();
+      return currentYear - person.year >= 19;
+      
+    });
+    console.log({allAdult});
+
+
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+    // AC: Used implicit return intead of if statement
+    const comment = comments.find(values => values.id === 823423);
+    console.log(comment);
+    
+
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // delete the comment with the ID of 823423
+    const index = comments.findIndex(values => values.id === 823423);
+    console.log(index);
+
+   //comments.splice(index, 1);
+   //or
+   //"spreading" new list of comments (deleted 1 item) into existing array so as not to make an array of arrays
+   const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+   ];
+   console.table(comments)
+   console.table(newComments);
+
+
+
+
+
+
+
+
+
+
+
